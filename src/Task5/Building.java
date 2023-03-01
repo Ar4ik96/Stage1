c1221478386065d486199e7476cd3a44b3118a49
Artur Eynikov <redmi1245ok@gmail.com>
2023-03-01 20:46:09 +0200

    Task5_1 Commit

--- src/Task5/Building.java
+++ src/Task5/Building.java
@@ -3,21 +3,34 @@ package Task5;
 import java.util.Scanner;
 
 public class Building {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите номер квартиры: ");
-        while (true) {
-            int apartment;
-            apartment = scanner.nextInt();
-            int entrance = (apartment - 1) / 36 + 1;
-            int storey = (apartment - 36 * (entrance - 1) - 1) / 4 + 1;
-            if (apartment > 0 && apartment < 145) {
-                System.out.println("Подъезд: " + entrance);
 
-                System.out.println("Этаж: " + storey);
-            } else {
-                System.out.println("В этом доме нет квартиры под этим номером!");
+
+        private static final int APARTMENTS = 144;
+        private static final int APARTMENTS_PER_HALL = 36;
+        private static final int APARTMENTS_PER_LEVEL = 4;
+        public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        while (true){
+            System.out.println("Введите номер квартиры: ");
+        int apartment = scanner.nextInt();
+        if ( apartment < 0 ||  apartment > APARTMENTS) {
+                System.out.println("B этом доме нет квартир под этим номером! ");
+                continue;
             }
-        }
+        int hall = (int) Math. ceil(apartment / (double) APARTMENTS_PER_HALL);
+        int apartmentInParticularHall = apartment % APARTMENTS_PER_HALL == 0
+                ? APARTMENTS_PER_HALL
+                : apartment % APARTMENTS_PER_HALL;
+        int level = (int) Math. ceil(apartmentInParticularHall / (double) APARTMENTS_PER_LEVEL);
+
+                System.out.println("Подъезд: " + hall);
+                System.out.println("Этаж: " + level);
     }
 }
+}
+
+//подъедов 4
+//этажей 9
+//квартир на этаже 4
+//квартир в подъеде 36
+//всего квартир 144
