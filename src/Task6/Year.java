67221e494284196eddbd68d4bdcd05766562d540
Artur Eynikov <redmi1245ok@gmail.com>
2023-03-03 09:48:32 +0200

    fixed variable day to year

--- src/Task6/Year.java
+++ src/Task6/Year.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Year {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int day = scanner.nextInt();
+        int  year = scanner.nextInt();
         System.out.println("Введите какой-то год: ");
-            int leapYear = day % 400;
-            int normalYear = day % 100;
-            int leapYearTo = day % 4;
+            int leapYear = year % 400;
+            int normalYear = year % 100;
+            int leapYearTo = year % 4;
             if (leapYearTo  < 1 && normalYear >= 1){
                 System.out.println("Это высокосный год в нем 366 дней...");
             }else if (leapYear < 1){
